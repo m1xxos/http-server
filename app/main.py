0f8485c4afb562a954aa0080d86e40e5d5059d5f
m1xxos <66390094+m1xxos@users.noreply.github.com>
2024-07-03 10:57:27 +0300

    feat:error and ok

--- app/main.py
+++ app/main.py
@@ -10,11 +10,16 @@ def main():
     #
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-        # conn, addr = server_socket.accept()  # wait for client
-        server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n") 
-
-        
-        # conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        conn, _ = server_socket.accept()  # wait for client
+        http_ok_message = b'HTTP/1.1 200 OK\r\n\r\n'
+        http_error_message = b'HTTP/1.1 404 Not Found\r\n\r\n'
+        with conn:
+            data = conn.recv(4096)
+            url = data.split()[1]
+            if url == b'/':
+                conn.sendall(http_ok_message)
+            else:
+                conn.sendall(http_error_message)
 
 
 if __name__ == "__main__":
