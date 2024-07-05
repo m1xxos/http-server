ab29691e82c0ac62f507fe1baae6dea837a7d95f
m1xxos <66390094+m1xxos@users.noreply.github.com>
2024-07-05 23:22:20 +0300

    feat:working with files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-# Uncomment this to pass the first stage
+import argparse
 import socket
 import re
 import threading
+from pathlib import Path
+
 
 HTTP_OK_MESSAGE = "HTTP/1.1 200 OK"
 HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found"
@@ -18,22 +20,43 @@ class Request:
         host_re = "Host:.*"
         accept_re = "Accept:.*"
         user_agent_re = "User-Agent:.*"
-        data = self._conn.recv(1024).decode()
+        data = self._recv_all()
+        if not data:
+            return "", "", "", ""
         splitted_data = data.split("\r\n")
         
-        _request = splitted_data[0] 
+        _request = splitted_data[0] if splitted_data else ""
         _host = self.filer_type(splitted_data, host_re)
         _accept = self.filer_type(splitted_data, accept_re)
         _user_agent = self.filer_type(splitted_data, user_agent_re)
         return _request, _host, _accept, _user_agent
     
     def get_url(self):
-        return self.request_info.split(" ")[1]
+        return self.request_info.split(" ")[1] if self.request_info else ""
 
     def filer_type(self, data, pattern):
         return next(filter(None, [re.findall(pattern, _) for _ in data]), "default")[0]
+    
+    def _recv_all(self, buffer_size=1024):
+        data = b''
+        while True:
+            part = self._conn.recv(buffer_size)
+            data += part
+            if len(part) < buffer_size:
+                break
+        return data.decode()
 
-def receive_connection(conn: socket):
+def read_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        return f"Файл '{file_path}' не найден."
+    except Exception as e:
+        return f"Ошибка при чтении файла: {e}"
+
+def receive_connection(conn: socket, file_folder):
     new_request = Request(conn)
     response = HTTP_NOT_FOUND + CRLF
     if new_request.url == '/':
@@ -46,20 +69,31 @@ def receive_connection(conn: socket):
         request_agent = new_request.user_agent.split(" ")[1]
         headers = f'Content-Type: text/plain\r\nContent-Length: {len(request_agent)}{CRLF}'
         response = f"{HTTP_OK_MESSAGE}\r\n{headers}{request_agent}{CRLF}"
+    elif '/files/' in new_request.url:
+        file_folder = Path(file_folder)
+        file_name = new_request.url.split("/")[-1]
+        file_path = file_folder / file_name
+        if file_path.is_file():
+            file_content = read_file(file_path)
+            headers = f'Content-Type: application/octet-stream\r\nContent-Length: {file_path.stat().st_size}{CRLF}'
+            response = f"{HTTP_OK_MESSAGE}\r\n{headers}{file_content}{CRLF}"
+
     print(response)
     conn.sendall(response.encode())
     # CodeCrafters fix
     conn.close() 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    parser = argparse.ArgumentParser(description="HTTP сервер")
+    parser.add_argument("--directory", default="/tmp/", help="Папка с файлами")
+    args = parser.parse_args()
+    file_folder = args.directory
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
         while True:
             conn, _ = server_socket.accept()  # wait for client
-            thread = threading.Thread(target=receive_connection, args=(conn,))
+            thread = threading.Thread(target=receive_connection, args=(conn,file_folder,))
             thread.start() # test
 
 if __name__ == "__main__":
